7d4c4e20692e1fe47de78a5e0c7c62efbb55f495
AndiCucka <124384763+AndiCucka@users.noreply.github.com>
2024-04-18 09:54:58 -0400

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,29 @@
+/*
+* The main program.
+*
+* @author  Andi Cucka
+* @version 1.0
+* @since   2024-04-13
+*/
+
+import { Airplane } from './Airplane'
+import { Jet } from './Jet'
+
+const biplane = new Airplane()
+biplane.setSpeed(212)
+console.log(biplane.getSpeed())
+const boeing = new Jet()
+boeing.setSpeed(422)
+console.log(boeing.getSpeed())
+let counter = 0
+while (counter < 4) {
+    boeing.accelerate()
+    console.log(boeing.getSpeed())
+    if (boeing.getSpeed() > 5000) {
+        biplane.setSpeed(biplane.getSpeed() * 2)
+    } else {
+        boeing.accelerate()
+    }
+  counter++
+}
+console.log(biplane.getSpeed())
